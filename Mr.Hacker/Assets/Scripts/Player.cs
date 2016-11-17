784b44d964cd05afa585bd0af348c5d4314a4fbf
MrTheGood <maarten@degoedeweb.nl>
2016-11-17 04:59:27 +0100

    Created the player and the robots[WIP].

---
+++ Mr.Hacker/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// @Author: Grafisch Lyceum Rotterdam, Maarten de Goede
+public class Player : MonoBehaviour {
+	public int health = 3;
+	public float speed = 4;
+
+	private CharacterController characterController;
+	private Animator animator;
+	private Sprite startSprite;
+
+
+
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+		startSprite = GetComponent<SpriteRenderer>().sprite;
+	}
+	void Update () {
+		//Move the player.
+		walkPlayer();
+	}
+
+
+	/// <summary>
+	/// Moves the player to the location based on the input, and plays the correct animation.
+	/// </summary>
+	private void walkPlayer() {
+		//Calculate the input/walking speed
+		float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+		//Reset the animator and rotation
+		transform.rotation = new Quaternion(0, 0, 0, 0);
+		animator.enabled = true;
+		//Test the direction and set the animation.
+		if (x > 0) {
+			//Right
+			animator.SetInteger("WalkState", 1);
+		} else if (x < 0) {
+			//Left
+			animator.SetInteger("WalkState", 1);
+			//Flip the player horizontally 
+			transform.rotation = new Quaternion(0, 180, 0, 0);
+		} else if (y > 0) {
+			//Up
+			animator.SetInteger("WalkState", 2);
+		} else if (y < 0) {
+			//Down
+			animator.SetInteger("WalkState", 0);
+		} else {
+			//The player isn't walking. Stop the animation and set a standing sprite.
+			animator.enabled = false;
+			GetComponent<SpriteRenderer>().sprite = startSprite;
+		}
+
+		//Move the player
+		characterController.Move(new Vector3(x, y, 0));
+	}
+}
---
+++ Mr.Hacker/Assets/Scripts/Robot.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// @Author: Grafisch Lyceum Rotterdam, Maarten de Goede
+public class Robot : MonoBehaviour {
+	public int health = 5;
+	public int damage = 1;
+	public float speed = 3;
+	public bool hackable = true;				//If the robot is hackable or not
+	public Vector2[] path;						//The path the robot should use
+
+	private int targetWaypoint;					//The next point of the path
+	private bool isHacked = false;				//If the robot is hacked
+	private Animator animator;
+
+
+	void Start () {
+		targetWaypoint = 0;						//The start target waypoint is 0
+		animator = GetComponent<Animator>();
+	}
+	void Update () {
+		//Move the robot
+		walkRobot(speed * Time.deltaTime);
+	}
+
+	/// <summary>
+	/// Fires a laser.
+	/// </summary>
+	private void fireLaser() {
+		//Star the animation
+		animator.SetTrigger("Fire");
+	}
+
+	/// <summary>
+	/// Walks the robot.
+	/// </summary>
+	/// <param name="speed">Moving speed.</param>
+	private void walkRobot(float speed) {
+		//Get the location where the robot should walk to.
+		Vector2 targetLocation = path[targetWaypoint];
+
+		//If the robot hasn't yet arrived at the target location,
+		if ((Vector2)transform.position != targetLocation) {
+			//Move towards the target location.
+			transform.position = Vector2.MoveTowards(transform.position, targetLocation, speed);
+			return;
+		}
+
+		//The robot has arrived. Let's go to the next waypoint.
+		targetWaypoint++;
+		//If the next waypoint doesn't exist,
+		if (targetWaypoint >= path.Length)
+			//Go to the first waypoint.
+			targetWaypoint = 0;
+	}
+}
